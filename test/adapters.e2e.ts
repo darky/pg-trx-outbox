9e200421decb787de05e8fd91b111600cd38fa59
Vladislav Botvin <darkvlados@gmail.com>
2023-05-09 18:31:58 +0300

    basic adapter cover

---
+++ test/adapters.e2e.ts
@@ -0,0 +1,184 @@
+import { afterEach, beforeEach, test } from 'node:test'
+import { PgTrxOutbox } from '../src'
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from 'testcontainers'
+import { Client } from 'pg'
+import assert from 'node:assert'
+import { OutboxMessage } from '../src/types'
+import { setTimeout } from 'timers/promises'
+import { SerialAdapter } from '../src/adapters/abstract/serial'
+import { ParallelAdapter } from '../src/adapters/abstract/parallel'
+
+let pgDocker: StartedPostgreSqlContainer
+let pg: Client
+let pgKafkaTrxOutbox: PgTrxOutbox
+
+beforeEach(async () => {
+  pgDocker = await new PostgreSqlContainer()
+    .withReuse()
+    .withCommand(['-c', 'fsync=off', '-c', 'wal_level=logical'])
+    .start()
+
+  pg = new Client({
+    host: pgDocker.getHost(),
+    port: pgDocker.getPort(),
+    user: pgDocker.getUsername(),
+    password: pgDocker.getPassword(),
+    database: pgDocker.getDatabase(),
+    application_name: 'pg_trx_outbox_admin',
+  })
+  await pg.connect()
+  await pg.query('DROP TABLE IF EXISTS pg_trx_outbox')
+  await pg.query(`
+    CREATE TABLE pg_trx_outbox (
+      id bigserial NOT NULL,
+      processed bool NOT NULL DEFAULT false,
+      created_at timestamptz NOT NULL DEFAULT now(),
+      updated_at timestamptz NOT NULL DEFAULT now(),
+      topic text NOT NULL,
+      "key" text NULL,
+      value jsonb NULL,
+      "partition" int2 NULL,
+      "timestamp" int8 NULL,
+      headers jsonb NULL,
+      response jsonb NULL,
+      error text NULL,
+      meta jsonb NULL,
+      context_id double precision NOT NULL DEFAULT random(),
+      CONSTRAINT pg_trx_outbox_pk PRIMARY KEY (id)
+    );
+  `)
+})
+
+afterEach(async () => {
+  await pgKafkaTrxOutbox.stop()
+  await pg.end()
+})
+
+test('SerialAdapter works', async () => {
+  pgKafkaTrxOutbox = new PgTrxOutbox({
+    adapter: new (class extends SerialAdapter {
+      async start() {}
+      async stop() {}
+      async handleMessage(message: OutboxMessage) {
+        if ((message.value as { ok: true }).ok) {
+          return { value: { success: true } }
+        }
+        throw new Error('err')
+      }
+    })(),
+    pgOptions: {
+      host: pgDocker.getHost(),
+      port: pgDocker.getPort(),
+      user: pgDocker.getUsername(),
+      password: pgDocker.getPassword(),
+      database: pgDocker.getDatabase(),
+    },
+    outboxOptions: {
+      pollInterval: 300,
+    },
+  })
+  await pgKafkaTrxOutbox.start()
+  await pg.query(
+    `
+      INSERT INTO pg_trx_outbox (topic, "key", value)
+      VALUES
+        ('pg.kafka.trx.outbox', 'testKey', '{"ok": true}'),
+        ('pg.kafka.trx.outbox', 'testKey', '{"err": true}')
+    `
+  )
+  await setTimeout(1000)
+
+  const resp = await pg.query<OutboxMessage>('select * from pg_trx_outbox order by id').then(r => r.rows)
+
+  assert.deepEqual(resp[0]?.response, { success: true })
+  assert.strictEqual(resp[0]?.error, null)
+
+  assert.strictEqual(resp[1]?.response, null)
+  assert.match(resp[1]?.error ?? '', /err/)
+})
+
+test('ParallelAdapter works', async () => {
+  pgKafkaTrxOutbox = new PgTrxOutbox({
+    adapter: new (class extends ParallelAdapter {
+      async start() {}
+      async stop() {}
+      async handleMessage(message: OutboxMessage) {
+        if ((message.value as { ok: true }).ok) {
+          return { value: { success: true } }
+        }
+        throw new Error('err')
+      }
+    })(),
+    pgOptions: {
+      host: pgDocker.getHost(),
+      port: pgDocker.getPort(),
+      user: pgDocker.getUsername(),
+      password: pgDocker.getPassword(),
+      database: pgDocker.getDatabase(),
+    },
+    outboxOptions: {
+      pollInterval: 300,
+    },
+  })
+  await pgKafkaTrxOutbox.start()
+  await pg.query(
+    `
+      INSERT INTO pg_trx_outbox (topic, "key", value)
+      VALUES
+        ('pg.kafka.trx.outbox', 'testKey', '{"ok": true}'),
+        ('pg.kafka.trx.outbox', 'testKey', '{"err": true}')
+    `
+  )
+  await setTimeout(1000)
+
+  const resp = await pg.query<OutboxMessage>('select * from pg_trx_outbox order by id').then(r => r.rows)
+
+  assert.deepEqual(resp[0]?.response, { success: true })
+  assert.strictEqual(resp[0]?.error, null)
+
+  assert.strictEqual(resp[1]?.response, null)
+  assert.match(resp[1]?.error ?? '', /err/)
+})
+
+test('GroupedAdapter works', async () => {
+  pgKafkaTrxOutbox = new PgTrxOutbox({
+    adapter: new (class extends ParallelAdapter {
+      async start() {}
+      async stop() {}
+      async handleMessage(message: OutboxMessage) {
+        if ((message.value as { ok: true }).ok) {
+          return { value: { success: true } }
+        }
+        throw new Error('err')
+      }
+    })(),
+    pgOptions: {
+      host: pgDocker.getHost(),
+      port: pgDocker.getPort(),
+      user: pgDocker.getUsername(),
+      password: pgDocker.getPassword(),
+      database: pgDocker.getDatabase(),
+    },
+    outboxOptions: {
+      pollInterval: 300,
+    },
+  })
+  await pgKafkaTrxOutbox.start()
+  await pg.query(
+    `
+      INSERT INTO pg_trx_outbox (topic, "key", value)
+      VALUES
+        ('pg.kafka.trx.outbox', 'testKey', '{"ok": true}'),
+        ('pg.kafka.trx.outbox', 'testKey', '{"err": true}')
+    `
+  )
+  await setTimeout(1000)
+
+  const resp = await pg.query<OutboxMessage>('select * from pg_trx_outbox order by id').then(r => r.rows)
+
+  assert.deepEqual(resp[0]?.response, { success: true })
+  assert.strictEqual(resp[0]?.error, null)
+
+  assert.strictEqual(resp[1]?.response, null)
+  assert.match(resp[1]?.error ?? '', /err/)
+})
