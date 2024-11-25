c55591b8d098b0952838d1df3977df41f5f8b0a6
Vladislav Botvin <darkvlados@gmail.com>
2024-11-25 18:52:11 +0300

    simplify fsm for notify

--- src/fsm.ts
+++ src/fsm.ts
@@ -7,11 +7,7 @@ export class FSM {
 
   private fsm = interpret(
     createMachine('wait', {
-      wait: state(
-        transition('poll', 'processing'),
-        transition('notify', 'processing'),
-        transition('repeat', 'processing')
-      ),
+      wait: state(transition('poll', 'processing'), transition('notify', 'processing')),
       processing: invoke(
         () => this.transfer.transferMessages(),
         transition('done', 'wait'),
@@ -19,20 +15,6 @@ export class FSM {
           'error',
           'wait',
           action((_, { error }: { error: Error }) => this.options.outboxOptions?.onError?.(error))
-        ),
-        transition('notify', 'waitRepeatProcessing')
-      ),
-      waitRepeatProcessing: state(
-        transition(
-          'done',
-          'wait',
-          action(() => process.nextTick(() => this.fsm.send('repeat')))
-        ),
-        transition(
-          'error',
-          'wait',
-          action((_, { error }: { error: Error }) => this.options.outboxOptions?.onError?.(error)),
-          action(() => process.nextTick(() => this.fsm.send('repeat')))
         )
       ),
     }),
