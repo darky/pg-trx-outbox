e4fd9d8b610ea48671f42eb523fa7cd4b7cc15be
Vladislav Botvin <darkvlados@gmail.com>
2025-02-26 14:41:45 +0300

    app name postfix

--- src/pg.ts
+++ src/pg.ts
@@ -1,12 +1,13 @@
 import { Pool } from 'pg'
 import type { Options, StartStop } from './types.ts'
+import { randomUUID } from 'node:crypto'
 
 export class Pg implements StartStop {
   private pool: Pool
 
   constructor(options: Options) {
     this.pool = new Pool({
-      application_name: 'pg_trx_outbox',
+      application_name: `pg_trx_outbox_${randomUUID().substring(0, 4).toLowerCase()}`,
       ...options.pgOptions,
     })
     this.pool.on('error', err => options.outboxOptions?.onError?.(err))
